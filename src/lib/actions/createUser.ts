415eb24aeadb5b40611fbed51b1080842229a123
ktwebsite <yancunjiantai@gmail.com>
2025-08-18 08:27:37 +0900

    flex createUser

--- src/lib/actions/createUser.ts
+++ src/lib/actions/createUser.ts
@@ -6,15 +6,17 @@ import { prisma } from '@/lib/prisma';
 import { registerSchema } from "@/validations/user"
 import bcryptjs from "bcryptjs"
 import { redirect } from 'next/navigation';
+import {ZodError} from "zod"
 // バリデーションエラー処理
-function handleValidationError(error: any): ActionState {
+function handleValidationError(error: ZodError): ActionState {
  const { fieldErrors, formErrors } = error.flatten();
+ const castedFieldErrors = fieldErrors as Record<string, string[]>; 
  // zodの仕様でパスワード一致確認のエラーは formErrorsで渡ってくる
 // formErrorsがある場合は、confirmPasswordフィールドにエラーを追加
  if (formErrors.length > 0) {
  return { success: false, errors: { ...fieldErrors, confirmPassword: formErrors
  }}}
- return { success: false, errors: fieldErrors };
+ return { success: false, errors: castedFieldErrors };
 }
 // カスタムエラー処理
 function handleError(customErrors: Record<string, string[]>): ActionState {
